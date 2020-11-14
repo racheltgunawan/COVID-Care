48963b3a15226dcb8e561823597e1b3d17b9fba4
Rachel Gunawan <rochitapiglet@gmail.com>
2020-11-13 17:21:26 -0800

    Initial Commit

---
+++ wei and rachel awesomeness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wei and rachel awesomeness
+//
+//  Created by Rachel Tiffany Gunawan on 11/13/20.
+//  Copyright © 2020 Rachel Tiffany Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
